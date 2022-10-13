7f02079b1554cb0604bd403eab82835cdc88b441
BnOiDev <bnoo.almeida@gmail.com>
2022-10-12 22:20:39 -0300

    news

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export function App() {
       <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
         Enviar
       </button>
+      
    </div>
   )
 }
--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,31 @@
-import { Text } from "./Text";
+import { Meta, StoryObj} from '@storybook/react';
+import { Text, TextProps } from './Text';
 
 export default {
     title: 'Componets/Text',
     component: Text,
-}
+    args:{
+        children: 'Lorem ipsum.',
+    }
+} as Meta<TextProps>
 
-export const Default = {}
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args:{
+        size:'sm',
+    }
+}
+export const Large: StoryObj<TextProps> = {
+    args:{
+        size:'lg',
+    } 
+}
+export const CustomComponent: StoryObj<TextProps> = {
+    args:{
+        asChild: true,
+        children:(
+            <p>Documentando Componentes</p>
+        ),
+    } 
+}
